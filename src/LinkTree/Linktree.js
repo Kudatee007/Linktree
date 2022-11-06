85806e075d11c4aa1f9d25d145a7936a293579fb
kudaT <timilehinkudaisi01@gmail.com>
2022-11-06 16:46:37 +0100

    bug:fixed linking bug

--- src/LinkTree/Linktree.js
+++ src/LinkTree/Linktree.js
@@ -7,20 +7,18 @@ import slack from "./img/slack.svg";
 import git from "./img/github.svg";
 import zuridesk from "./img/Footer/Zuri.Internship_Logo.svg";
 import ingressdesk from "./img/Footer/I4G.svg";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Linktree = () => {
   return (
     <div className="LinkTree">
       {/* slack username */}
 
-      <h1 id="slack">
-        timilehinkudaisi01
-      </h1>
+      <h1 id="slack">timilehinkudaisi01</h1>
 
       {/* profile picture */}
 
-      <img src={proPics} alt="" id="profile__img"  />
+      <img src={proPics} alt="" id="profile__img" />
 
       {/* share Btn  */}
       <img src={shareBtn} alt="" id="share" />
@@ -40,7 +38,10 @@ const Linktree = () => {
         <a href="http://books.zuri.team" id="books">
           Zuri Books
         </a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<timilehinkudaisi01>" id="book__python">
+        <a
+          href="https://books.zuri.team/python-for-beginners?ref_id=<timilehinkudaisi01>"
+          id="book__python"
+        >
           Python Books
         </a>
         <a href="https://background.zuri.team" id="pitch">
@@ -49,20 +50,25 @@ const Linktree = () => {
         <a href="https://books.zuri.team/design-rules" id="book__design">
           Design Books
         </a>
-        <Link to="/contact"> <a href="" id="contact">
+
+        <a href="/contact" id="contact">
           Contact Me
-        </a></Link>
+        </a>
       </div>
       <div className="desktop-View">
         <div className="icons">
-          <a href="timilehinkudaisi01"><img src={slack} alt="slack" /></a>
-          <a href="https://github.com/Kuldatee"><img src={git} alt="github" /></a>
+          <a href="timilehinkudaisi01">
+            <img src={slack} alt="slack" />
+          </a>
+          <a href="https://github.com/Kuldatee">
+            <img src={git} alt="github" />
+          </a>
         </div>
         <hr className="line" />
         <footer className="footer">
-            <img src={zuridesk} alt="" className="zurii"/>
-            <h2 className="intern">HNG Internship 9 Frontend Task</h2>
-            <img src={ingressdesk} alt="" className="ingress"/>
+          <img src={zuridesk} alt="" className="zurii" />
+          <h2 className="intern">HNG Internship 9 Frontend Task</h2>
+          <img src={ingressdesk} alt="" className="ingress" />
         </footer>
       </div>
     </div>
